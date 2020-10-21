1dc5839ff3c774fe4f022b30d106ad9e11de1512
Andrzej <mucha.andrzej94@gmail.com>
2020-10-21 23:41:29 +0200

    created auth repozsitory

---
+++ MyFinances.API/MyFinances.API/Interfaces/IAuthRepository.cs
@@ -0,0 +1,12 @@
+﻿using MyFinances.API.Models;
+using System.Threading.Tasks;
+
+namespace MyFinances.API.Interfaces
+{
+    interface IAuthRepository
+    {
+        Task<User> Login(string email, string password);
+        Task<User> Regiser(User user, string password);
+        Task<bool> UserExist(string email);
+    }
+}
---
+++ MyFinances.API/MyFinances.API/Repositories/AuthRepository.cs
@@ -0,0 +1,63 @@
+﻿using MyFinances.API.Data;
+using MyFinances.API.Interfaces;
+using MyFinances.API.Models;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Text;
+
+namespace MyFinances.API.Repositories
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly DataContext _context;
+
+        public AuthRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> Login(string email, string password)
+        {
+            User user = await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+            if (user == null)
+                return null;
+
+            if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt)) {
+                return null;
+            }
+            return user;
+
+        }
+
+        public Task<User> Regiser(User user, string password)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> UserExist(string email)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        #region private 
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] paswordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(paswordSalt))
+            {
+                var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    if (computedHash[i] != passwordHash[i])
+                        return false;
+                }
+                return true;
+            }
+
+
+        }
+
+        #endregion
+    }
+}
